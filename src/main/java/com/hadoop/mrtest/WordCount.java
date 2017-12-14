ba7ae4708493d5c5b329d04a9437a185bc512a33
wangzy0327 <wangzy0327@qq.com>
2017-12-14 19:16:02 +0800

    add log4j.properties and modify FileSystem

--- src/main/java/com/hadoop/mrtest/WordCount.java
+++ src/main/java/com/hadoop/mrtest/WordCount.java
@@ -1,6 +1,7 @@
 package com.hadoop.mrtest;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
@@ -14,6 +15,13 @@ import java.io.IOException;
 import java.util.Iterator;
 
 public class WordCount  {
+
+    //输入文件相对路径
+    private static String inPath = "/user/wzy/input/dream.txt";
+    //输出文件相对路径
+    private static String outPath = "/user/wzy/output";
+    //hdfs文件地址
+    private static String hdfs ="hdfs://localhost:9000";
 
     public static class TokenizerMapper extends
             Mapper<Object, Text, Text, IntWritable> {
@@ -58,15 +66,21 @@ public class WordCount  {
     public static void main(String[] args)
             throws IOException, ClassNotFoundException, InterruptedException {
 
-        FileUtil.deleteDir("output");
         Configuration conf = new Configuration();
+        //设置hdfs地址
+        conf.set("fs.defaultFS",hdfs);
+        FileSystem fs = FileSystem.get(conf);
+        Path inputPath = new Path(inPath);
+        Path outputPath = new Path(outPath);
+
+        FileUtil.deleteDir("output");
 
         //String[] otherArgs = new String[]{"input/dream.txt","output"};
-        String[] otherArgs = new String[]{"hdfs://localhost:9000/user/wzy/input/dream.txt","hdfs://localhost:9000/user/wzy/output"};
-        if (otherArgs.length != 2) {
-            System.err.println("Usage:Merge and duplicate removal <in> <out>");
-            System.exit(2);
-        }
+//        String[] otherArgs = new String[]{"hdfs://localhost:9000/user/wzy/input/dream.txt","hdfs://localhost:9000/user/wzy/output"};
+//        if (otherArgs.length != 2) {
+//            System.err.println("Usage:Merge and duplicate removal <in> <out>");
+//            System.exit(2);
+//        }
 
         Job job = Job.getInstance(conf, "WordCount");
         job.setJarByClass(WordCount.class);
@@ -74,8 +88,12 @@ public class WordCount  {
         job.setReducerClass(WordCount.IntSumReduce.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
-        FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
-        FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+        FileInputFormat.addInputPath(job, inputPath);
+
+        //如果路径存在则删除，否则不删除
+        fs.delete(outputPath,true);
+
+        FileOutputFormat.setOutputPath(job, outputPath);
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
