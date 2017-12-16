d40ab6a9f58b8644e4e45331f057ced77214b312
wangzy0327 <wangzy0327@qq.com>
2017-12-16 13:53:57 +0800

    format code on v2.0

--- src/main/java/com/hadoop/matrix/step1/Mapper1.java
+++ src/main/java/com/hadoop/matrix/step1/Mapper1.java
@@ -6,13 +6,14 @@ import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
 
-public class Mapper1 extends Mapper<LongWritable,Text,Text,Text> {
+public class Mapper1 extends Mapper<LongWritable, Text, Text, Text> {
     private Text outKey = new Text();
     private Text outValue = new Text();
 
     /**
      * key:1
      * value:1   1_0,2_3,3_-1,4_2,5_-3
+     *
      * @param key
      * @param value
      * @param context
@@ -25,17 +26,17 @@ public class Mapper1 extends Mapper<LongWritable,Text,Text,Text> {
         String[] rowAndLine = value.toString().split("\\t");
         //矩阵的行号
         String row = rowAndLine[0];
-        System.out.println("矩阵行号:"+row);
+        System.out.println("矩阵行号:" + row);
         System.out.println(rowAndLine[1]);
         String[] line = rowAndLine[1].split(",");
         //1_0,2_3,3_-1,4_2,5_-3
-        for(int i=0;i<line.length;i++){
+        for (int i = 0; i < line.length; i++) {
             String column = line[i].split("_")[0];
             String valueStr = line[i].split("_")[1];
             //key:列号   value:行号_值
             outKey.set(column);
-            outValue.set(row+"_"+valueStr);
-            context.write(outKey,outValue);
+            outValue.set(row + "_" + valueStr);
+            context.write(outKey, outValue);
         }
     }
 }
